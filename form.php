716371ea5819835a5d5085e0f5a43758e2bbff0e
Florian <florianchanchus83@gmail.com>
2022-02-03 14:39:45 +0100

    C1

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Depot Photo</title>
+</head>
+<body>
+​
+    <main>
+​
+​
+    </main>
+​
+    <h1>Depot photos</h1>
+​
+    <form action="process.php" method="post" enctype="multipart/form-data">
+    
+        <div>
+            <input type="file" name="photo">
+​
+        </div>
+        <input type="submit" value="Envoyer"/>
+        
+​
+    </form>
+    
+</body>
